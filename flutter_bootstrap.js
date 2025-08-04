78354125a5309fa5932f208bc150835c35a6046e
viku4 <viku4@users.noreply.github.com>
2025-08-04 05:41:01 +0000

    deploy: 3d5f66eb9b58d56d9c5ceb9267dfaf6ab32ed0dd

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -39,6 +39,6 @@ _flutter.buildConfig = {"engineRevision":"ef0cd000916d64fa0c5d09cc809fa7ad244a57
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "3799652474"
+    serviceWorkerVersion: "3189971588"
   }
 });
